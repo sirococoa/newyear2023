3ce13ad41d4e5f8a25187f12d48b20ecc3d5b191
sirococoa <torakia.mcml178@gmail.com>
2022-12-16 22:44:01 +0900

    [add] create carrot base program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyxel
+from random import randint, random
 
 WINDOW_WIDTH = 256
 WINDOW_HEIGHT = 256
@@ -9,14 +10,23 @@ class App:
         pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT)
         pyxel.load('./assets/my_resource.pyxres')
         self.rabbit = Rabbit()
+        self.carrots = []
         pyxel.run(self.update, self.draw)
 
     def update(self):
         self.rabbit.update()
+        for carrot in self.carrots:
+            carrot.update(1)
+        if random() < 0.3:
+            self.carrots.append(Carrot())
+        self.carrots = [carrot for carrot in self.carrots if carrot.alive]
+        print(len(self.carrots))
 
     def draw(self):
         pyxel.cls(0)
         self.rabbit.draw()
+        for carrot in self.carrots:
+            carrot.draw()
 
 
 class Rabbit:
@@ -44,6 +54,24 @@ class Rabbit:
         pyxel.blt(self.x, self.y, 0, self.RUN_U, self.RUN_V, self.RUN_W, self.RUN_H, 0)
 
 
+class Carrot:
+    U = 9
+    V = 80
+    W = 16
+    H = 32
+
+    def __init__(self):
+        self.x = WINDOW_WIDTH
+        self.y = randint(0, WINDOW_HEIGHT) // self.H * self.H
+        self.alive = True
+
+    def update(self, scroll_speed):
+        if self.x < -self.W:
+            self.alive = False
+        self.x -= scroll_speed
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, self.U, self.V, self.W, self.H, 0)
 
 
 App()
